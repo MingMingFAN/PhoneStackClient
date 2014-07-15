c0c15a0bdcb9c546c3d4e2929c369f5e770b3de9
MingMingFAN <mfan@uncc.edu>
2014-07-15 11:12:38 -0700

    client socket thread

---
+++ PhoneStackServer/src/mingming/research/ClientThread.java
@@ -0,0 +1,72 @@
+package mingming.research;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientThread implements Runnable {
+
+	Socket mSocket;
+	int id;
+	PrintWriter output = null;
+	BufferedReader input = null;
+	
+	public ClientThread(Socket _socket, int _id)
+	{
+		mSocket = _socket;
+		id = _id;
+	}
+	
+	public void run() {
+		// TODO Auto-generated method stub
+ 
+		if(mSocket != null)
+		{
+			if(!mSocket.isClosed())
+			{
+				try {
+				    output = new PrintWriter(mSocket.getOutputStream(),true);
+					input = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+					System.out.println("a client connects... ");
+					while(!mSocket.isClosed())
+					{
+						if(input != null && input.ready())
+						{
+							String inputData = input.readLine();
+							System.out.println("data from client: " + id + " is: " + inputData);
+						}
+					}
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else
+			{
+				if(input != null)
+				{
+					try {
+						input.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					input = null;
+					
+				}
+				
+				if(output != null)
+				{
+					output.close();
+					output = null;
+				}
+			}
+		}
+
+		
+	}
+
+}
